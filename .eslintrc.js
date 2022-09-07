b2c0990c0810553d4ea10b80f758892485201ded
Artem Rys <rysartem@gmail.com>
2022-09-07 18:01:39 +0200

    feat: add new default file with no input

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
     extends: '@splunk/eslint-config/browser-prettier',
+    env: {
+        'browser': true,
+        'amd': true,
+        'node': true,
+    },
     globals: {
         __non_webpack_require__: true,
     },
