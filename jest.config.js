c8273fa94c071d6bed7df6e386d5b00b784a6750
Tushar Balar <tbalar@splunk.com>
2023-01-11 08:56:55 +0000

    test(UI): ADDON-58762 Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 /* eslint no-undef: "error" */
 /* eslint-env node */
 module.exports = {
+<<<<<<< HEAD
+=======
+    /* eslint no-undef: "error" */
+>>>>>>> f9bbe7e (test(UI): ADDON-58762 Jest setup)
     // Mock
     clearMocks: true,
 
