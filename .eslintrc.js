9db8fd9867c1a31e0b019fd33d55718a5b5787ae
Tushar Balar <tbalar@splunk.com>
2023-01-11 09:02:42 +0000

    test(UI): ADDON-58762 Update eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 /* eslint no-undef: "error" */
 /* eslint-env node */
 module.exports = {
-<<<<<<< HEAD
     parser: '@babel/eslint-parser',
-=======
-    /* eslint no-undef: "error" */ parser: '@babel/eslint-parser',
->>>>>>> f9bbe7e (test(UI): ADDON-58762 Jest setup)
     extends: ['@splunk/eslint-config/browser', 'prettier', 'plugin:jest/recommended'],
     plugins: ['prettier', 'jest'],
     env: {
