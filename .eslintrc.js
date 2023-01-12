39077b0c2db603d00176bccba528a583ecc2be2c
Tushar Balar <tbalar@splunk.com>
2023-01-12 12:29:38 +0000

    fix ADDON-58705 Table action buttons are dynamic from the global config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,7 @@
 /* eslint no-undef: "error" */
 /* eslint-env node */
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
     parser: '@babel/eslint-parser',
-=======
-    /* eslint no-undef: "error" */ parser: '@babel/eslint-parser',
->>>>>>> f9bbe7e (test(UI): ADDON-58762 Jest setup)
-=======
-    parser: '@babel/eslint-parser',
->>>>>>> cfb61c9 (test(UI): ADDON-58762 Update eslintrc file)
     extends: ['@splunk/eslint-config/browser', 'prettier', 'plugin:jest/recommended'],
     plugins: ['prettier', 'jest'],
     env: {
