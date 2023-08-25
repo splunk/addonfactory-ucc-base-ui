f59e9eeab8250d532a3ddf7a40c5e10dbf712cad
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2023-08-25 14:49:04 +0200

    chore(deps): add msw for API mocking
    ADDON-64637

---
+++ src/main/webapp/mocks/server-response.js
@@ -0,0 +1,18 @@
+export const mockServerResponse = {
+    links: {
+        create: '/servicesNS/nobody/Splunk_TA_aws/splunk_ta_aws_aws_billing_cur/_new',
+    },
+    origin: 'https://10.202.12.58:8000/servicesNS/nobody/Splunk_TA_aws/splunk_ta_aws_aws_billing_cur',
+    updated: '2023-08-21T11:54:12+00:00',
+    generator: {
+        build: '1c86ca0bacc3',
+        version: '9.1.0',
+    },
+    entry: [],
+    paging: {
+        total: 0,
+        perPage: 18446744073709552000,
+        offset: 0,
+    },
+    messages: [],
+};
