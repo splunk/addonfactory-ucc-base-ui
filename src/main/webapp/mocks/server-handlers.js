0397aad71f672cbfacc600644958e77325b55eb6
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2023-08-30 11:58:31 +0200

    chore(deps): ADDON-64637 removed hardcoded names

--- src/main/webapp/mocks/server-handlers.js
+++ src/main/webapp/mocks/server-handlers.js
@@ -1,13 +1,11 @@
 // src/mocks/serverHandlers.js
 import { rest } from 'msw';
-import { mockServerResponse } from './server-response';
+import { MOCKED_TA_INPUT, mockServerResponse } from './server-response';
 
 export const serverHandlers = [
     rest.get('/servicesNS/nobody/-/mockGeneratedEndPointUrl', (req, res, ctx) =>
         res(ctx.json(mockServerResponse))
     ),
-    rest.get('/servicesNS/nobody/-/splunk_ta_aws_aws_all_accounts', (req, res, ctx) =>
-        res(ctx.status(200))
-    ),
+    rest.get(`/servicesNS/nobody/-/${MOCKED_TA_INPUT}`, (req, res, ctx) => res(ctx.status(200))),
     rest.get('/servicesNS/nobody/-/data/indexes', (req, res, ctx) => res(ctx.status(200))),
 ];
--- src/main/webapp/mocks/server-response.js
+++ src/main/webapp/mocks/server-response.js
@@ -1,18 +1,11 @@
+export const MOCKED_TA_NAME = 'Splunk_TA_mocked_name';
+export const MOCKED_TA_INPUT = 'splunk_ta_mocked_input_name';
+
 export const mockServerResponse = {
     links: {
-        create: '/servicesNS/nobody/Splunk_TA_aws/splunk_ta_aws_aws_billing_cur/_new',
+        create: `/servicesNS/nobody/${MOCKED_TA_NAME}/${MOCKED_TA_INPUT}/_new`,
     },
-    origin: 'https://10.202.12.58:8000/servicesNS/nobody/Splunk_TA_aws/splunk_ta_aws_aws_billing_cur',
     updated: '2023-08-21T11:54:12+00:00',
-    generator: {
-        build: '1c86ca0bacc3',
-        version: '9.1.0',
-    },
     entry: [],
-    paging: {
-        total: 0,
-        perPage: 18446744073709552000,
-        offset: 0,
-    },
     messages: [],
 };
