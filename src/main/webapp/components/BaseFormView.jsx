597bf678746efca8697b7d8a36390d59a53546c8
Szymon Oleksy <soleksy@splunk.com>
2023-09-27 21:52:30 +0200

    fix: adding remove file while cloning
    adding remove file possibility while doing cloning process

--- src/main/webapp/components/BaseFormView.jsx
+++ src/main/webapp/components/BaseFormView.jsx
@@ -279,6 +279,14 @@ class BaseFormView extends PureComponent {
                         typeof e?.options?.display !== 'undefined' ? e.options.display : true;
                     tempEntity.error = false;
                     tempEntity.disabled = false;
+                    if (e.type === 'file') {
+                        /* 
+                         adding example name as we want the possibility to remove this file
+                         not forcing value addition as if value is encrypted it is shared as
+                         string ie. ***** and it is considered a valid default value
+                        */
+                        tempEntity.fileNameToDisplay = 'Previous File';
+                    }
                     temState[e.field] = tempEntity;
                 } else if (props.mode === MODE_CONFIG) {
                     e.defaultValue = typeof e.defaultValue !== 'undefined' ? e.defaultValue : null;
